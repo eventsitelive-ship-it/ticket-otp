5164d7588e9cc7ebc1fff8d93f99fcce57acc15f
eventsitelive-ship-it <eventsitelive@gmail.com>
2025-10-07 00:16:56 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,15 @@
-// firebase-config.js
-// Paste your Firebase config values below:
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
 // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUoOZqzb8IK8wn5TqVa-5GEYuo7ThrU9A",
+  authDomain: "ticketotp-75fa0.firebaseapp.com",
+  projectId: "ticketotp-75fa0",
+  storageBucket: "ticketotp-75fa0.firebasestorage.app",
+  messagingSenderId: "375782482549",
+  appId: "1:375782482549:web:a9f5020dc7685f76f5ce3c",
+  measurementId: "G-8HX1M1X3KS"
+};
 firebase.initializeApp(firebaseConfig);
