b8085f4aac9d2479e5bc46e5bbac9f636e29694d
eventsitelive-ship-it <eventsitelive@gmail.com>
2025-10-07 00:23:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,13 @@
-// Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
-
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDUoOZqzb8IK8wn5TqVa-5GEYuo7ThrU9A",
   authDomain: "ticketotp-75fa0.firebaseapp.com",
   projectId: "ticketotp-75fa0",
-  storageBucket: "ticketotp-75fa0.firebasestorage.app",
+  storageBucket: "ticketotp-75fa0.appspot.com",
   messagingSenderId: "375782482549",
   appId: "1:375782482549:web:a9f5020dc7685f76f5ce3c",
   measurementId: "G-8HX1M1X3KS"
 };
+
+// Initialize Firebase (for non-module HTML setup)
 firebase.initializeApp(firebaseConfig);
